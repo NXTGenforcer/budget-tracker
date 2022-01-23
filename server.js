4708d67ffbcf59f7e8e0f5372479b2f8f757408e
NXTGenforcer <gskywalker11@gmail.com>
2022-01-23 15:13:14 -0500

    deploying

--- server.js
+++ server.js
@@ -16,8 +16,10 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
   useFindAndModify: false,
 });
 
