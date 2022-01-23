10e871385bf0b99140726456bc609a8b057fd5fe
NXTGenforcer <gskywalker11@gmail.com>
2022-01-23 16:30:52 -0500

    deployment fixes

--- public/dist/indexedDB.bundle.js
+++ public/dist/indexedDB.bundle.js
@@ -7,60 +7,26 @@
  * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
  */
 /******/ (() => { // webpackBootstrap
-/******/ 	"use strict";
 /******/ 	var __webpack_modules__ = ({
 
 /***/ "./assets/js/indexed-db.js":
 /*!*********************************!*\
   !*** ./assets/js/indexed-db.js ***!
   \*********************************/
-/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+/***/ (() => {
 
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"checkForIndexedDb\": () => (/* binding */ checkForIndexedDb),\n/* harmony export */   \"useIndexedDb\": () => (/* binding */ useIndexedDb)\n/* harmony export */ });\nfunction checkForIndexedDb() {\n  if (!window.indexedDB) {\n    console.log(\"Your browser doesn't support a stable version of IndexedDB.\");\n    return false;\n  }\n\n  return true;\n}\nfunction useIndexedDb(databaseName, storeName, method, object) {\n  return new Promise(function (resolve, reject) {\n    var request = window.indexedDB.open(databaseName, 1);\n    var db, tx, store;\n\n    request.onupgradeneeded = function (e) {\n      var db = request.result;\n      db.createObjectStore(storeName, {\n        keyPath: \"_id\"\n      });\n    };\n\n    request.onerror = function (e) {\n      console.log(\"There was an error\");\n    };\n\n    request.onsuccess = function (e) {\n      db = request.result;\n      tx = db.transaction(storeName, \"readwrite\");\n      store = tx.objectStore(storeName);\n\n      db.onerror = function (e) {\n        console.log(\"error\");\n      };\n\n      if (method === \"put\") {\n        store.put(object);\n      } else if (method === \"get\") {\n        var all = store.getAll();\n\n        all.onsuccess = function () {\n          resolve(all.result);\n        };\n      } else if (method === \"delete\") {\n        store[\"delete\"](object._id);\n      }\n\n      tx.oncomplete = function () {\n        db.close();\n      };\n    };\n  });\n}\n\n//# sourceURL=webpack://public/./assets/js/indexed-db.js?");
+eval("var indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDb || window.msIndexedDB || window.shimIndexedDB;\nvar request = indexedDB.open(\"budgetTracker\", 1);\nvar db, tx, store;\n\nrequest.onupgradeneeded = function (e) {\n  var db = e.target.result;\n  db.createObjectStore(\"pending\", {\n    autoIncrement: true\n  });\n};\n\nrequest.onerror = function (e) {\n  console.log(\"There was an error\", e.target.errorCode);\n};\n\nrequest.onsuccess = function (e) {\n  db = e.target.result;\n\n  if (navigator.onLine) {\n    checkForIndexedDb();\n  }\n};\n\nfunction saveRecord(record) {\n  var tx = db.transaction([\"pending\"], \"readwrite\");\n  var store = tx.objectStore(\"pending\");\n  store.add(record);\n}\n\nfunction checkForIndexedDb() {\n  tx = db.transaction([\"pending\"], \"readwrite\");\n  store = tx.objectStore(\"pending\");\n  var allBudget = store.getAll();\n\n  allBudget.onsuccess = function () {\n    if (allBudget.result.length > 0) {\n      fetch(\"/api/transaction/bulk\", {\n        method: \"POST\",\n        body: JSON.stringify(allBudget.result),\n        headers: {\n          Accept: \"application/json, text/plain, */*\",\n          \"Content-Type\": \"application/json\"\n        }\n      }).then(function (res) {\n        return res.json();\n      }).then(function () {\n        var tx = db.transaction([\"pending\"], \"readwrite\");\n        var store = tx.objectStore(\"pending\");\n        store.clear();\n      });\n    }\n  };\n}\n\nwindow.addEventListener(\"online\", checkForIndexedDb);\n\n//# sourceURL=webpack://public/./assets/js/indexed-db.js?");
 
 /***/ })
 
 /******/ 	});
-/************************************************************************/
-/******/ 	// The require scope
-/******/ 	var __webpack_require__ = {};
-/******/ 	
-/************************************************************************/
-/******/ 	/* webpack/runtime/define property getters */
-/******/ 	(() => {
-/******/ 		// define getter functions for harmony exports
-/******/ 		__webpack_require__.d = (exports, definition) => {
-/******/ 			for(var key in definition) {
-/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
-/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
-/******/ 				}
-/******/ 			}
-/******/ 		};
-/******/ 	})();
-/******/ 	
-/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
-/******/ 	(() => {
-/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
-/******/ 	})();
-/******/ 	
-/******/ 	/* webpack/runtime/make namespace object */
-/******/ 	(() => {
-/******/ 		// define __esModule on exports
-/******/ 		__webpack_require__.r = (exports) => {
-/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
-/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
-/******/ 			}
-/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
-/******/ 		};
-/******/ 	})();
-/******/ 	
 /************************************************************************/
 /******/ 	
 /******/ 	// startup
 /******/ 	// Load entry module and return exports
 /******/ 	// This entry module can't be inlined because the eval devtool is used.
 /******/ 	var __webpack_exports__ = {};
-/******/ 	__webpack_modules__["./assets/js/indexed-db.js"](0, __webpack_exports__, __webpack_require__);
+/******/ 	__webpack_modules__["./assets/js/indexed-db.js"]();
 /******/ 	
 /******/ })()
 ;
